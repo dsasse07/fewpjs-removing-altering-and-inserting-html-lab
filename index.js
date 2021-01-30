b58ef3e3f2a10d79fe020833573cafae99206475
Daniel Sasse <dsasse07@gmail.com>
2021-01-30 17:12:12 -0500

    commit tree 6b1f98e9e2d0b9ac199bd72a8b8e0cb1fd715854

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let main = document.querySelector('main#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>"
